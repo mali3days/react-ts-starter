f8750fe229676214959c3fd1135a7365a5c66777
Nikolay Bugay <nikolay.bugay@nixsolutions.com>
2019-04-11 23:55:20 +0300

    update project settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export interface Props {
+  blabla?: string;
 }
 
-export default App;
+export const App: React.FC<Props> = ({ blabla }: Props): JSX.Element => {
+  const [name, setName] = React.useState(blabla);
+
+  const onSubmit = (e: React.SyntheticEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
+
+  const updateName = (e: React.SyntheticEvent<HTMLInputElement>): void => {
+    setName(e.currentTarget.value);
+  };
+
+  return (
+    <div className="App">
+      <form noValidate={true} autoComplete="off" onSubmit={onSubmit}>
+        <input type="text" value={name} onChange={updateName} />
+      </form>
+    </div>
+  );
+};
