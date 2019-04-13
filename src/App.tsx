46614ae02a624fa5667afac43232f422c0e1779d
Nikolay Bugay <nikolay.bugay@nixsolutions.com>
2019-04-13 19:09:26 +0300

    :heavy_plus_sign: Add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import * as React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+
 import './App.css';
 
-export interface Props {
-  blabla?: string;
-}
+// export interface Props {
+//   // blabla?: string;
+// }
 
-export const App: React.FC<Props> = ({ blabla }: Props): JSX.Element => {
-  const [name, setName] = React.useState(blabla);
+export const App: React.FC<{}> = (): JSX.Element => {
+  const [name, setName] = React.useState('');
 
   const onSubmit = (e: React.SyntheticEvent<HTMLFormElement>): void => {
     e.preventDefault();
   };
 
-  const updateName = (e: React.SyntheticEvent<HTMLInputElement>): void => {
+  const updateName = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setName(e.currentTarget.value);
   };
 
   return (
     <div className="App">
       <form noValidate={true} autoComplete="off" onSubmit={onSubmit}>
-        <input type="text" value={name} onChange={updateName} />
+        <TextField
+          id="standard-full-width"
+          label="New word"
+          style={{ margin: 8 }}
+          placeholder="Watermelon"
+          helperText="Enter new word and press `Enter`"
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          value={name}
+          onChange={updateName}
+        />
       </form>
     </div>
   );
